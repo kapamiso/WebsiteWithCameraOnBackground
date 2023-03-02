94af3e327147fc068509e6be868b85692f077ac8
kapamiso <kapamiso@gmail.com>
2023-03-02 01:35:46 -0300

    Fixing ios cam bug

--- index.js
+++ index.js
@@ -1,5 +1,36 @@
+async function getMediaStream() {
+  var mediaStream = null;
+
+  var constraints = {
+    audio: false,
+    video: {
+      width: { ideal: 640 },
+      height: { ideal: 480 },
+      facingMode: "environment",
+    },
+  };
+
+  try {
+    mediaStream = await navigator.mediaDevices.getUserMedia(constraints);
+    let video = document.querySelector("#camera");
+    video.srcObject = mediaStream;
+    video.onloadedmetadata = (event) => {
+      video.play();
+    };
+
+    let video2 = document.querySelector("#camera-2");
+    video2.srcObject = mediaStream;
+    video2.onloadedmetadata = (event) => {
+      video2.play();
+    };
+    
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
 function startCamera() {
-  const configs = { video: {} };
+  const configs = { video: true };
 
   navigator.mediaDevices
     .getUserMedia(configs)
@@ -22,4 +53,4 @@ function startCamera() {
     });
 }
 
-window.addEventListener("DOMContentLoaded", startCamera);
+window.addEventListener("DOMContentLoaded", getMediaStream());
